97168e9646de1d7a7cb75d7af8e4540efd94376d
Ash Simmonds <ashsimmonds@gmail.com>
2022-07-11 16:48:40 +0930

    hacky shift and pop to remove fluff

--- routes/api/quote.ts
+++ routes/api/quote.ts
@@ -2,20 +2,26 @@ import { HandlerContext } from "$fresh/server.ts";
 
 const redditPost = await fetch('https://www.reddit.com/r/EntrepreneurRideAlong/comments/vw6yeo/47_sentences_thatll_make_you_more_money_than_a_4/.json');
 
-
-// read the post body and separate into sentences
 const postBody = await redditPost.json();
-const sentences = postBody[0].data.children[0].data.selftext.split('.');
 
-// pick a random sentence
-const randomSentence = sentences[Math.floor(Math.random() * sentences.length)];
+let sentences = postBody[0].data.children[0].data.selftext.split('.');
 
-// console.log(randomSentence);
+sentences = sentences.map(sentence => sentence.replace(/\n\n/g, '') + '.');
+
+sentences.shift();
+sentences.pop();
+sentences.pop();
+sentences.pop();
+
+console.log(sentences);
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
-    // const randomIndex = Math.floor(Math.random() * QUOTES.length);
-    const body = randomSentence; //QUOTES[randomIndex];
+
+    const randomSentence = sentences[Math.floor(Math.random() * sentences.length)];
+
+    const body = randomSentence;
+
     console.log(body);
-    // return new Response(JSON.stringify('asdf'));
+
     return new Response(body);
 };
